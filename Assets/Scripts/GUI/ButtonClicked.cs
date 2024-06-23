88d58e0bc3edc04230777acc4fa746147d240cd1
SefaSelim <sefaselimtura@gmail.com>
2024-06-24 01:28:20 +0300

    Main Menu UI Trials

---
+++ Assets/Scripts/GUI/ButtonClicked.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClicked : MonoBehaviour
+{
+    private Animator animator;
+            float timer = 0f;
+            public bool onclick;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+       public void playAnim()
+    {
+        animator.SetBool("İsClicked", true);
+    }
+
+
+private void Update() {
+    if (onclick)
+    {
+              timer += Time.deltaTime;
+
+        if (timer > 0.25f)
+        {
+            timer = 0f;
+            animator.SetBool("İsClicked", false);
+            onclick = false;
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+
+}
+
+    public void setfalse()
+    {
+        onclick = true;
+    }
+}
+
+
